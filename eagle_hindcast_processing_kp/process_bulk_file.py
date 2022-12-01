c4158552882486086309a4d12f49a08485538250
Katie Peterson <107277894+katieapeterson@users.noreply.github.com>
2022-12-01 16:50:18 -0700

    Update process_bulk_file.py

--- eagle_hindcast_processing_kp/process_bulk_file.py
+++ eagle_hindcast_processing_kp/process_bulk_file.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
 
 	dataDir = '/projects/hindcastra/filepurgatory/WestCoast'
 	saveDir = '/scratch/kpeterso2/wave_processing/westcoast'
-	year = '2011'
+	year = '2012'
 
 	# run main execution
 	main(dataDir,saveDir,year)
