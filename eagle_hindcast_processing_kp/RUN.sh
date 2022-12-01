f5ef57ad580b6fb683586ee88fa61c3e5f400115
Katie Peterson <107277894+katieapeterson@users.noreply.github.com>
2022-12-01 13:31:50 -0700

    Update RUN.sh

--- eagle_hindcast_processing_kp/RUN.sh
+++ eagle_hindcast_processing_kp/RUN.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --account=hindcastra
 #SBATCH --time=4:00:00
-#SBATCH --job-name=Ak2020raw
+#SBATCH --job-name=WC2020raw
 #SBATCH --nodes=1
 #SBATCH --mem=128GB
 #SBATCH --qos=high
 #SBATCH --ntasks-per-node=1
-#SBATCH --mail-user=Aidan.Bharath@nrel.gov
+#SBATCH --mail-user=katie.peterson@nrel.gov
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=output/output_%j.out
 #SBATCH --error=error/error_%j.err
 
 
-srun python /projects/hindcastra/cloud_wave_data/process_raw/alaska/process_nc/process_bulk_file.py $1
+srun python /projects/hindcastra/filepurgatory/WestCoast/process_bulk_file.py $1
