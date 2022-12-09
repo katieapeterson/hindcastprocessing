d259e07ce765c28789ae7489d666c3fbe45211a8
Katie Peterson <107277894+katieapeterson@users.noreply.github.com>
2022-12-09 12:53:57 -0700

    Update process_bulk_file.py

--- eagle_hindcast_processing_kp/process_bulk_file.py
+++ eagle_hindcast_processing_kp/process_bulk_file.py
@@ -104,7 +104,7 @@ def clean_name(sd):
 
 	sd: root save directory
 	"""
-	saveFile = f'{sd}/American_Samoa_wave_{Year}.h5'	
+	saveFile = f'{sd}/Jarvis_Island_wave_{Year}.h5'	
 	if os.path.isfile(saveFile):
 		os.remove(saveFile)
 	return saveFile
@@ -175,8 +175,8 @@ def main(*args, **kwargs):
 if __name__ == "__main__":
 
 
-	dataDir = '/projects/hindcastra/filepurgatory/americanSamoa'
-	saveDir = '/scratch/kpeterso2/wave_processing/americansamoa'
+	dataDir = '/projects/hindcastra/filepurgatory/jarvisIsland'
+	saveDir = '/scratch/kpeterso2/wave_processing/jarvisisland'
 	year = ['1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020']
 
 	# run main execution
