d2c257b676ba2c2f74eca04d1ff1750a303341be
Katie Peterson <107277894+katieapeterson@users.noreply.github.com>
2022-12-20 12:09:17 -0700

    Update RUN.sh

--- eagle_hindcast_processing_kp/RUN.sh
+++ eagle_hindcast_processing_kp/RUN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=hindcastra
 #SBATCH --time=4:00:00
-#SBATCH --job-name=JAtollraw
+#SBATCH --job-name=CNMIraw
 #SBATCH --nodes=1
 #SBATCH --mem=128GB
 #SBATCH --qos=high
