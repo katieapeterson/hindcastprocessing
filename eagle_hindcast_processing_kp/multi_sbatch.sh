f3f8b8afc207497bc486d17c4f41d8b1ab4a4397
Katie Peterson <107277894+katieapeterson@users.noreply.github.com>
2023-01-30 12:25:25 -0700

    Update multi_sbatch.sh

--- eagle_hindcast_processing_kp/multi_sbatch.sh
+++ eagle_hindcast_processing_kp/multi_sbatch.sh
@@ -1,5 +1,5 @@
 for i in $(seq 2011 1 2020)
 do
-	sbatch /scratch/kpeterso2/wave_processing/eagle_hindcast_processing_kp/RUN.sh $i
+	sbatch /scratch/kpeterso2/wave_processing/hindcastprocessing/eagle_hindcast_processing_kp/RUN.sh $i
 	echo $i
 done
